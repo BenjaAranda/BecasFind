bea44042cfc2388fe33de9c55c0937b776e387e2
benja <be.arandaa@duocuc.cl>
2026-05-10 01:37:04 -0400

    feat: completar fase 6 con data seeding de pruebas

--- backend/src/main/java/com/becasfind/api/repositories/BecaRepository.java
+++ backend/src/main/java/com/becasfind/api/repositories/BecaRepository.java
@@ -4,12 +4,13 @@ import com.becasfind.api.models.entities.Beca;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BecaRepository extends JpaRepository<Beca, Long> {
+public interface BecaRepository extends JpaRepository<Beca, Long>, JpaSpecificationExecutor<Beca> {
 
     @Query("SELECT DISTINCT b FROM Beca b " +
             "LEFT JOIN FETCH b.institucion i " +
