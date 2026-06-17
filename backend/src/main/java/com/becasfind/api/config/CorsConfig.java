922f9def904a308dfc3c3026412b4cc7dbcd52b7
José Naour <jo.naour@duocuc.cl>
2026-06-17 15:38:23 -0400

    Revert "Cambios para CI/CD"
    This reverts commit 57c69d25358fd452d965ee1870c26a0034f70153.

--- backend/src/main/java/com/becasfind/api/config/CorsConfig.java
+++ backend/src/main/java/com/becasfind/api/config/CorsConfig.java
@@ -11,7 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(
-                        "https://becasfind.onrender.com",
                         "http://localhost:5173",
                         "http://localhost:3000"
                 )
